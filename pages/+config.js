01c69f5e35ba58f565565a43362b8c85f3d81172
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-10-30 11:45:06 -0300

    feat: vercel config

--- pages/+config.js
+++ pages/+config.js
@@ -1,9 +1,10 @@
 // https://vike.dev/config
 import vikeReact from 'vike-react/config'
+import vikePhoton from 'vike-photon/config'
 
 export default {
   // https://vike.dev/extends
-  extends: vikeReact,
+  extends: [vikeReact, vikePhoton],
   // https://vike.dev/clientRouting
   clientRouting: true,
   // https://vike.dev/hydrationCanBeAborted
